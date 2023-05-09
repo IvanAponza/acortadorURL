b288ade9db40dc69be3c7ae5f8f68fa89f01c771
Ivan Aponza <joselito881218@hotmail.com>
2023-05-09 16:29:14 -0500

    se integra el motor de plantilla handlebars y se configura la estructura de las carpetas para las vistas

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
+const { create } = require('express-handlebars');
 require('dotenv').config()
 require('./database/db.js')
 
@@ -8,9 +9,19 @@ const app = express();
 
 //Configuraciones | Settings
 const PORT = process.env.PORT || 4000
+const hbs = create({
+    extname: '.hbs',
+    partialsDir: ['views/components']
+})
+app.engine('hbs', hbs.engine);
+app.set('view engine', '.hbs');
+app.set('views', './views');
 
 //Middlewares
 app.use(morgan('dev')) //muestra todas las peticiones hechas al server
+app.use(express.static(__dirname + 'public'));
+app.use(express.urlencoded({ extended: true }));
+
 
 //Rutas | Routes
 app.use("/", require('./routes/home'));
